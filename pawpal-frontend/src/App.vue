c2aa460d547254d49534ac9bee052f660fe56f12
aanxniee <caishajia@gmail.com>
2023-08-30 17:47:44 -0400

    created + styled vertical navbar

--- pawpal-frontend/src/App.vue
+++ pawpal-frontend/src/App.vue
@@ -1,6 +1,91 @@
 <template>
+  <div class="flex">
+    <nav
+      class="py-10 bg-stone-100 px-4 border-r border-gray-300 h-screen flex flex-col justify-between items-center align-middle"
+    >
+      <div class="menu-top">
+        <a href="#"
+          ><img src="/logo_pawpal.svg" class="object-scale-down h-16 w-16"
+        /></a>
+      </div>
 
-  <RouterView />
+      <div class="menu-center align-middle flex flex-col gap-5 space-y-4">
+        <RouterLink to="/feed" class="text-blue-900">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+            />
+          </svg>
+        </RouterLink>
+
+        <RouterLink to="/search">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+            ></path>
+          </svg>
+        </RouterLink>
+        <RouterLink to="/chat">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z"
+            />
+          </svg>
+        </RouterLink>
+
+        <RouterLink to="/notifications">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+            ></path>
+          </svg>
+        </RouterLink>
+      </div>
+
+      <div class="menu-bottom">
+        <a href="#">
+          <img src="logo.svg" class="rounded-full" />
+        </a>
+      </div>
+    </nav>
+    <main class="px-8 py-6 bg-stone-100 flex-1">
+      <RouterView />
+    </main>
+  </div>
 </template>
-
-<style scoped></style>
