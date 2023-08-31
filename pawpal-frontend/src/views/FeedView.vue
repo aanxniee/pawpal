f5237d023fec475e80335e2b491b0cacf757ed48
aanxniee <caishajia@gmail.com>
2023-08-30 21:12:25 -0400

    started feedings view

---
+++ pawpal-frontend/src/views/FeedView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="max-w-7xl mx-auto grid grid-cols-4 gap-4">
+    <div class="main-center col-span-3 space-y-4">
+      <div class="p-4 bg-stone-100 border border-gray-200 rounded-lg">
+        <p>create a posting</p>
+      </div>
+
+      <div class="p-4 bg-stone-100 border border-gray-200 rounded-lg">
+        <p>a post</p>
+      </div>
+    </div>
+
+    <div class="main-right col-span-1 space-y-4">
+      <p>sidebar with people and trends</p>
+    </div>
+  </div>
+</template>
